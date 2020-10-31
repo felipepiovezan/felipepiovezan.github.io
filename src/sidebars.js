d0db292e4e69d048d2759c94426c3c894cd6fcd9
Felipe de Azevedo Piovezan <piovezan.fpi@gmail.com>
2020-10-30 20:32:59 -0400

    add static var post

--- src/sidebars.js
+++ src/sidebars.js
@@ -2,6 +2,7 @@ module.exports = {
   someSidebar: {
     Posts : [
     '20200503-git_intro/README',
+    '20200306-static_var/README',
     '20191117-improve_spam/README',
     {
       type: 'link',
