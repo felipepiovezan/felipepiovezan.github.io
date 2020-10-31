6a2551eac00f143f4d50f48eb3849cf49107a6d1
Felipe de Azevedo Piovezan <piovezan.fpi@gmail.com>
2020-10-30 20:41:43 -0400

    add build system posts

--- src/sidebars.js
+++ src/sidebars.js
@@ -3,6 +3,8 @@ module.exports = {
     Posts : [
     '20200503-git_intro/README',
     '20200306-static_var/README',
+    '20200222-build_systems2/README',
+    '20200127-build_systems1/README',
     '20191117-improve_spam/README',
     {
       type: 'link',
