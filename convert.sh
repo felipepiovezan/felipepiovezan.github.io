0a3198261878926a3795a94b0ef239603af1e231
Felipe de Azevedo Piovezan <piovezan.fpi@gmail.com>
2022-03-26 09:19:30 -0400

    add "home" link as a top-bar

--- convert.sh
+++ convert.sh
@@ -10,12 +10,16 @@ table_of_contents="--toc --include-in-header=$PWD/templates/toc.css "
 css="--variable=fontcolor:white --variable=backgroundcolor:black"
 css=""
 
+add_home_buttom="--include-in-header=$PWD/templates/top_bar.css
+                 --include-before-body=$PWD/templates/blog_home_link.html"
+
 pandoc_flags="-f markdown -t html -s
               --fail-if-warnings
               --self-contained
               ${table_of_contents}
               ${css}
-              ${highlight_style}"
+              ${highlight_style}
+              ${add_home_buttom}"
 
 for md_path in ${md_files[@]}; do
   dir_name=$(dirname $md_path)
