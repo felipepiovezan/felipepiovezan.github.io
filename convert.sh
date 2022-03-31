c1bf2a607c5bfad6b193a728f66654163a8c8b39
Felipe de Azevedo Piovezan <piovezan.fpi@gmail.com>
2022-03-31 06:09:57 -0400

    test usage of og properties for website preview

--- convert.sh
+++ convert.sh
@@ -21,10 +21,22 @@ pandoc_flags="-f markdown -t html -s
               ${highlight_style}
               ${add_home_buttom}"
 
+# Returns "--include-in-header=header.html" if the file exists in `md_dir`.
+get_header_flag() {
+  md_dir=$1
+  header_name="header.html"
+  header_path=${md_dir}/${header_name}
+  if test -f "${header_path}"; then
+      echo "--include-in-header=${header_name}"
+  fi
+  echo ""
+}
+
 for md_path in ${md_files[@]}; do
   dir_name=$(dirname $md_path)
   md_name=$(basename $md_path)
+  header_flag="$(get_header_flag ${dir_name})"
 
   (cd ${dir_name} &&
-   pandoc ${pandoc_flags} ${md_name} -o index.html)
+   pandoc ${pandoc_flags} ${header_flag} ${md_name} -o index.html)
 done
